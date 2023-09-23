d4ac9ba06b15be8a8d6a0fda515354c663f1e257
danielmaangi <danielmaangi@gmail.com>
2023-09-23 19:13:44 +0300

    project name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'KCM Oversight Data System'
+copyright = '2023, Kenya Coordinating Mechanism'
+author = 'Daniel Maangi'
 
 release = '0.1'
 version = '0.1.0'
